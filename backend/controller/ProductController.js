fa15830284e6e0ea79cf5db5153db983b1d30495
NtapeKnoxSiwale <ntape.k.siwale@gmail.com>
2022-11-08 14:05:46 +0200

    Products API (GET, DELETE, PUT and UPDATE)

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -1,5 +1,73 @@
-exports.getAllProducts = (req, res) => {
+const Product = require("../models/ProductModel.js");
+
+// Creating the Product
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// Getting all Products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
   res.status(200).json({
     message: "Route is working fine",
+    products,
+  });
+};
+
+// Updating Product ---Administrator
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    //return next(new ErrorHandler("Product with this ID is not found", 404));
+    res.status(200).json({
+      success: false,
+      message: "Product is not found with this ID",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useUnified: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Product deleting
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: true,
+      message: "Product with this ID is not found.",
+    });
+  }
+
+  await product.remove();
+  return res.status(200).json({
+    success: true,
+    message: "The Product was deleted successfully.",
+  });
+};
+
+//Single Product Details
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: true,
+      message: "Product with this ID is not found.",
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    product,
   });
 };
---
+++ backend/models/ProductModel.js
@@ -0,0 +1,98 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter the Name of your Product"],
+    trim: true,
+    maxlength: [
+      25,
+      "The product name should not exceed more than 25 characters",
+    ],
+  },
+  description: {
+    type: String,
+    required: [true, "Enter the Description of your Product"],
+    maxlength: [
+      4000,
+      "Description should not exceed more than 4000 characters",
+    ],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please add a price for your Product"],
+    maxlength: [8, "Price can not exceed more than 8 characters"],
+  },
+  discountPrice: {
+    type: String,
+    maxlength: [4, "Discount price can not exceed more than 4 characters"],
+  },
+  color: {
+    type: String,
+  },
+  size: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please add a category of your Product"],
+  },
+  Stock: {
+    type: Number,
+    required: [true, "Please add some stock for your Product"],
+    maxlength: [4, "Stock can not exceed more than 3 Characters"],
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  review: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+      },
+      time: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
