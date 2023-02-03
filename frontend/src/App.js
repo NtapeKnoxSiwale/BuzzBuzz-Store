89591e5fb416c272b653200e8874f99bbe1f4212
NtapeKnoxSiwale <ntape.k.siwale@gmail.com>
2023-02-03 13:07:19 +0200

    .env update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
-import "./App.css";
-import Header from "./component/Home/Header";
+import { useEffect } from "react";
+import WebFont from "webfontloader";
 
-function App() {
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/Home/Home";
+import ProductDetails from "./pages/Products/ProductDetails";
+
+const App = () => {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Montserrat", "Roboto"],
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/product/:id" component={ProductDetails} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
